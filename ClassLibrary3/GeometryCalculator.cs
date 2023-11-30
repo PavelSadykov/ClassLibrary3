d7a3ba55f867000bdd079a206541515f11db67ae
PavelSadykov <2439747@gmail.com>
2023-11-30 22:34:58 +0500

    Добавьте файлы проекта.

---
+++ ClassLibrary3/GeometryCalculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary3
+{
+    public class GeometryCalculator
+    {
+        public static double CalculateSquareArea(double sideLength)
+        {
+            return sideLength * sideLength;
+        }
+        public static double CalculateRectangleArea(double length, double width)
+        {
+            return length * width;
+        }
+        public static double CalculateTriangleeArea(double baseLength, double heidht)
+        {
+            return 0.5 * baseLength * heidht;
+        }
+
+
+    }
+}
